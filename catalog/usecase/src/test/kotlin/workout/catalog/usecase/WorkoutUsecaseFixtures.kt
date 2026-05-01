9349c77b2b0b83b46cdff7744042740a67cefc01
evgeny <evgenv2015@gmail.com>
2026-05-01 11:08:43 +0500

    удалил импорты из WorkoutUsecaseFixtures.kt

--- catalog/usecase/src/test/kotlin/workout/catalog/usecase/WorkoutUsecaseFixtures.kt
+++ catalog/usecase/src/test/kotlin/workout/catalog/usecase/WorkoutUsecaseFixtures.kt
@@ -1,10 +1,5 @@
 package workout.catalog.usecase
 
-import workout.catalog.usecase.AddWorkoutUseCase
-import workout.catalog.usecase.MockIdStore
-import workout.catalog.usecase.MockSaveWorkout
-import workout.catalog.usecase.MockWorkoutAlreadyExist
-
 fun addWorkoutUsecase(): AddWorkoutUseCase {
     val idStore = MockIdStore()
     val saveWorkout = MockSaveWorkout()
