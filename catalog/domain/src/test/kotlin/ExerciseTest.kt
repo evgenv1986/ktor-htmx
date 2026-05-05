1c0112604b5c17b3456420c39f9559e84633c790
evgeny <evgenv2015@gmail.com>
2026-05-06 00:03:47 +0500

    создание упражнения из парсера упражнения

--- catalog/domain/src/test/kotlin/ExerciseTest.kt
+++ catalog/domain/src/test/kotlin/ExerciseTest.kt
@@ -1,4 +1,6 @@
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 
 class ExerciseTest: StringSpec({
     "should extract name from simple string"{
@@ -8,10 +10,38 @@ class ExerciseTest: StringSpec({
             """.trimIndent()
         ).name().equals( ParsedExercise("Подтягивания"))
     }
+    "should create exercise from parsed exercise"{
+        ParsedExercise(
+            """
+                1. Подтягивания
+            """.trimIndent()
+        ).toExercise().shouldBeInstanceOf<Exercise>()
+        ParsedExercise(
+            """
+                1. Подтягивания
+            """.trimIndent()
+        ).toExercise().name() shouldBe "Подтягивания"
+        ParsedExercise(
+            """
+                1. Подтягивания
+            """.trimIndent()
+        ).toExercise().equals(Exercise("Подтягивания"))
+    }
 })
+
+class Exercise(val name: String) {
+    fun name(): String {
+        return name
+    }
+
+}
 
 open class ParsedExercise(val value: String) {
     fun name(): String {
         return value.replace("1. ", "").trim()
+    }
+
+    fun toExercise(): Exercise {
+        return Exercise(name())
     }
 }
