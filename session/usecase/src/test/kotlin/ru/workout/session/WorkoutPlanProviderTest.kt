9fdceecc05028d18f4ffe16d1bdd9deb777507a5
evgeny <evgenv2015@gmail.com>
2026-05-16 08:52:20 +0500

    дополнил тест проверяющий поставку плана тренировки, проверкой результата поставки плана упражнений

--- session/usecase/src/test/kotlin/ru/workout/session/WorkoutPlanProviderTest.kt
+++ session/usecase/src/test/kotlin/ru/workout/session/WorkoutPlanProviderTest.kt
@@ -1,14 +1,19 @@
 package ru.workout.session
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.string.shouldContain
 import java.util.UUID
 
 class WorkoutPlanProviderTest: StringSpec( {
     "mock workout plan provider should load workout plan by contract"{
         val mockProvider: WorkoutPlanProvider =
             MockWorkoutPlanProvider(WorkoutPlan(
-                workoutId: UUID = UUID.randomUUID(),
+                workoutPlanId: UUID = UUID.randomUUID(),
                 listOf("Подтягивания", "Отжимания")
             ))
+        val result = mockProvider.WorkoutPlanById(workoutPlanId = UUID.randomUUID())
+        val plan = result.shouldBeRight()
+        plan shouldContain "Подтягивания"
+        plan shouldContain "Отжимания"
     }
 })
