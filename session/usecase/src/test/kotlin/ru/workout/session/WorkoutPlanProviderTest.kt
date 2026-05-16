3bdb652781fccb19dd3ab6f807dd479d4951165b
evgeny <evgenv2015@gmail.com>
2026-05-16 09:10:10 +0500

    мок реализация поставки плана тренировки и выполняющийся тест

--- session/usecase/src/test/kotlin/ru/workout/session/WorkoutPlanProviderTest.kt
+++ session/usecase/src/test/kotlin/ru/workout/session/WorkoutPlanProviderTest.kt
@@ -1,19 +1,45 @@
 package ru.workout.session
 
+import arrow.core.Either
+import arrow.core.right
+import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.shouldBe
 import java.util.UUID
+
+data class WorkoutPlan(val workoutPlanId: UUID, val exercises: List<String>)
+interface WorkoutPlanProvider {
+    fun workoutPlanById(workoutPlanId: UUID): Either<WorkoutProviderError, WorkoutPlan>
+    fun verifyInvoked(planId: UUID)
+}
+
+sealed interface WorkoutProviderError {
+}
 
 class WorkoutPlanProviderTest: StringSpec( {
     "mock workout plan provider should load workout plan by contract"{
+        val planId = UUID.randomUUID()
+        val expectedExercises = listOf("Подтягивания", "Отжимания")
         val mockProvider: WorkoutPlanProvider =
-            MockWorkoutPlanProvider(WorkoutPlan(
-                workoutPlanId: UUID = UUID.randomUUID(),
-                listOf("Подтягивания", "Отжимания")
-            ))
-        val result = mockProvider.WorkoutPlanById(workoutPlanId = UUID.randomUUID())
+            MockWorkoutPlanProvider(
+                WorkoutPlan(planId, expectedExercises))
+        val result = mockProvider.workoutPlanById(planId)
         val plan = result.shouldBeRight()
-        plan shouldContain "Подтягивания"
-        plan shouldContain "Отжимания"
+        plan.exercises shouldBe expectedExercises
+        mockProvider.verifyInvoked(planId)
     }
 })
+
+open class MockWorkoutPlanProvider(val workoutPlan: WorkoutPlan): WorkoutPlanProvider {
+    private lateinit var planId: UUID
+
+    override fun workoutPlanById(workoutPlanId: UUID): Either<WorkoutProviderError, WorkoutPlan> {
+        this.planId = workoutPlanId
+        return workoutPlan.right()
+    }
+
+    override fun verifyInvoked(planId: UUID) {
+        planId shouldBe this.planId
+    }
+}
+
