8a65953d94ce7da3ed256948f190a5a0591a7e30
evgeny <evgenv2015@gmail.com>
2026-06-16 23:04:58 +0500

    создал тест класс для добавочной сессии

--- application/src/test/kotlin/ru/workout/session/usecase/AdditionalSessionTest.kt
+++ application/src/test/kotlin/ru/workout/session/usecase/AdditionalSessionTest.kt
@@ -1,4 +1,28 @@
 package ru.workout.session.usecase
 
-class AdditionalSessionTest {
-}
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import ru.workout.session.domain.SessionStatus
+import ru.workout.session.usecase.additional.AdditionalSession
+import ru.workout.session.usecase.additional.AdditionalTask
+import ru.workout.session.usecase.additional.AdditionalTaskStatus
+
+class AdditionalSessionTest: StringSpec({
+    "can complete step"
+//    "думаю сессию делать отдельно, в другом классе теста. могу посмотреть количество оставшегося времени выполнения упражнения - через сессию"
+    {
+        val handStand = taskHandstand(targetTime = 300)
+        handStand.remainsCompleted() shouldBe 300
+        handStand.status() shouldBe AdditionalTaskStatus.PLANNED
+
+        val session = AdditionalSession(
+            tasks = listOf(taskHandstand()),
+            status = SessionStatus.IN_PROGRESS
+        )
+        session.completeStep(
+            task = taskWithFirstStepCompleted(),
+            step = step()
+        )
+
+    }
+})
