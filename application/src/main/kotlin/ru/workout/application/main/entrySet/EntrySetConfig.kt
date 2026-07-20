c4b4dc71f6fb63346162e0b4ea4df99e693beeee
evgeny <evgenv2015@gmail.com>
2026-07-20 13:25:22 +0500

    обработка отправки выполненного подхода по задаче

--- application/src/main/kotlin/ru/workout/application/main/entrySet/EntrySetConfig.kt
+++ application/src/main/kotlin/ru/workout/application/main/entrySet/EntrySetConfig.kt
@@ -3,13 +3,18 @@ package ru.workout.application.main.entrySet
 import io.ktor.server.application.call
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.put
 import ru.workout.rest.ENTRY_SET
 import ru.workout.rest.ENTRY_SET_NEW
 import ru.workout.session.rest.set.EntrySetEndpoint
+import ru.workout.session.rest.set.HandleAddSetEndpoint
 
 fun Route.entrySetConfig() {
     get(ENTRY_SET_NEW) {
         EntrySetEndpoint(call).handle(call)
+    }
+    post(ENTRY_SET){
+        HandleAddSetEndpoint(call).handle()
     }
 }
